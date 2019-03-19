11f6c637543ed322daad71f8d847102aa2746f11
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-19 17:16:04 +0800

    icons

--- project-test/travel/src/pages/home/components/Icons.vue
+++ project-test/travel/src/pages/home/components/Icons.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="icons">
     <div class="icon">
-      <img src="" />
-      <p></p>
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+      </div>
+      <p class="icon-desc">景点门票</p>
     </div>
   </div>
 </template>
@@ -14,12 +16,38 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/variables.styl'
   .icons
     height: 0
     overflow: hidden
     padding-bottom: 50%
     .icon
+      position: relative
+      overflow: hidden
       float: left
       width: 25%
+      height: 0
       padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding-top: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        font-size: .24rem
 </style>
