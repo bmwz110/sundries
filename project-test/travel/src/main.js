fc7c92436e1157b2036a553aebbb57553613950f
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-19 13:58:28 +0800

    20190319

--- project-test/travel/src/main.js
+++ project-test/travel/src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick' // 某些移动端浏览器的click事件会延迟300ms执行，通过引入fastclick来撤销这一默认操作
+import './assets/styles/reset.css' // reset.css保证在所有浏览器上显示效果一致
+import './assets/styles/border.css' // border.css用于实现1像素边框
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
