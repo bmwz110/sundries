0223adf0ec0e8913e22e961b6a2443516ab87898
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-19 16:53:09 +0800

    icons

--- project-test/travel/src/pages/home/Home.vue
+++ project-test/travel/src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header' // Header是保留字，不能作为组件名，所以存为HomeHeader
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
---
+++ project-test/travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <img src="" />
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .icons
+    height: 0
+    overflow: hidden
+    padding-bottom: 50%
+    .icon
+      float: left
+      width: 25%
+      padding-bottom: 25%
+</style>
