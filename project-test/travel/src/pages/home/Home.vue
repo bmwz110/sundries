a4ff5e596377ffb05c79927cf65c3bdd4b51eaa6
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-20 09:37:08 +0800

    recommend

--- project-test/travel/src/pages/home/Home.vue
+++ project-test/travel/src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HomeHeader from './components/Header' // Header是保留字，不能作为组件名，所以存为HomeHeader
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
