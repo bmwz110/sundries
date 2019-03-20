088103fed5a1d83d2bc48f7e09751a0653b7f187
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-20 10:34:14 +0800

    recommend

--- project-test/travel/src/pages/home/components/Recommend.vue
+++ project-test/travel/src/pages/home/components/Recommend.vue
@@ -2,13 +2,11 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item">
-        <div class="item-img-wrapper">
-          <img class="item-img" src="" />
-        </div>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" />
         <div class="item-info">
-          <p class="item-title"></p>
-          <p class="item-desc"></p>
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
       </li>
@@ -18,7 +16,22 @@
 
 <script>
 export default {
-  name: 'HomeRecommend'
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/9a/9a44ea713b93140d90.water.jpg_200x200_dcf0f049.jpg',
+        title: '西溪国家湿地公园·周家村',
+        desc: '杭州西溪国家湿地公园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/9a/9a44ea713b93140d90.water.jpg_200x200_dcf0f049.jpg',
+        title: '西溪国家湿地公园·周家村',
+        desc: '杭州西溪国家湿地公园'
+      }]
+    }
+  }
 }
 </script>
 
@@ -28,4 +41,27 @@ export default {
     line-height: .8rem
     background: #eee
     text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+      .item-button
+        margin-top: .2rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
 </style>
---
+++ project-test/travel/src/pages/home/components/Weekend.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/7d45cc38259e0.jpg_r_640x214_156f4299.jpg',
+        title: '承载杭城的历史之地',
+        desc: '饱经沧桑的地方诉述着这座城的段段衷肠'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+        title: '美丽普陀山',
+        desc: '人间仙境杭州，佛教圣地普陀山等你来玩！在这里静心接收佛家的洗礼，用心感受佛文化'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.4%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+</style>
