8edb58bca91c4bcf45e8e0968df88406ae19dc69
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-20 09:42:45 +0800

    recommend

--- project-test/travel/src/pages/home/components/Recommend.vue
+++ project-test/travel/src/pages/home/components/Recommend.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <div class="title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <div class="item-img-wrapper">
+          <img class="item-img" src="" />
+        </div>
+        <div class="item-info">
+          <p class="item-title"></p>
+          <p class="item-desc"></p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
