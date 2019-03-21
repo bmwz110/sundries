9342d80bcd01dafcd2b0d79d9ce91b660cecc408
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-21 16:40:01 +0800

    list

--- project-test/travel/src/pages/home/components/Swiper.vue
+++ project-test/travel/src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper">
       <swiper :options="swiperOption">
-        <swiper-slide v-for="item of swiperList" :key="item.id">
+        <swiper-slide v-for="item of list" :key="item.id">
           <img class="swiper-img" :src="item.imgUrl" />
         </swiper-slide>
         <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,19 +12,15 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination', // pagination(分页)属性：.类名 --> 添加分页标志
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/278a88f18f1523eec8c52f650522be68.jpg_750x200_bc365244.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/2df3ba0f23801d7ed5607f4767a18482.jpg_750x200_b4d6aa93.jpg'
-      }]
+      }
     }
   }
 }
