857c3ccc1b87f321e097e002657675e52fb9c445
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-30 11:09:59 +0800

    add

--- project-test/travel/src/router/index.js
+++ project-test/travel/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
 
 Vue.use(Router)
 
@@ -12,6 +13,6 @@ export default new Router({
   }, {
     path: '/city',
     name: 'City',
-    component: Home
+    component: City
   }]
 })
