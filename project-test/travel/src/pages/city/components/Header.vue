ee60b68a6dfbd19dfffb3a8a18a4a42e5187f38c
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-31 15:37:17 +0800

    test

--- project-test/travel/src/pages/city/components/Header.vue
+++ project-test/travel/src/pages/city/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
     城市选择
+    <div class="iconfont header-icon">&#xe624;</div>
   </div>
 </template>
 
@@ -11,12 +12,21 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  @import '~style/variables.styl'
+  @import '~styles/variables.styl'
   .header
+    position: relative
     overflow: hidden
     height: .86rem
     line-height: .86rem
     text-align: center
     color: #fff
     background: $bgColor
+    font-size: .32rem
+    .header-icon
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
 </style>
