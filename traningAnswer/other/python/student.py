43a3613cbe53745f4a0d86c42930bfd506d00955
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-04-25 09:47:49 +0800

    student

--- traningAnswer/other/python/student.py
+++ traningAnswer/other/python/student.py
@@ -1,14 +1,15 @@
-from human import human
+from human import Human
 
 class Student(Human):
   def __init__(self, school, name, age):
     self.school = school
     super(Student, self).__init__(name, age)
-    
+
   def do_homework(self):
     print('English homework')
 
-student1 = Student('人民路小学'，'小明', 18)
+student1 = Student('人民路小学','小明', 18)
+student1.do_homework()
 print(student1.school)
 print(student1.name)
 print(student1.age) 
